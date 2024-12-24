d4bbf7383d3def741c2f01025c11de88af9c42b4
David FYON <davidfyon@gmail.com>
2024-12-24 09:21:48 +0100

    PageContainer

---
+++ pages/page-container.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { PageContainer } from '@cnamts/synapse'
+</script>
+
+<template>
+	<PageContainer>
+		Contenu de la page
+	</PageContainer>
+
+	<br/>
+
+	<PageContainer size="s">
+		Contenu de la page
+	</PageContainer>
+
+	<br/>
+
+	<PageContainer color="primary">
+		<div class="pa-8">
+			Contenu de la page
+		</div>
+	</PageContainer>
+
+	<br/>
+
+	<PageContainer>
+		<VCard class="pa-8">
+			Contenu de la page
+		</VCard>
+	</PageContainer>
+</template>
