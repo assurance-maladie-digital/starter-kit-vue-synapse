e78e53c13783d53799c7a40cba39e27a37cac59b
David FYON <davidfyon@gmail.com>
2024-12-26 11:53:43 +0100

    add skiplink

---
+++ pages/skip-link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { SkipLink } from '@cnamts/synapse'
+</script>
+
+<template>
+	<div class="pa-8">
+		<p>Pour afficher le composant, cliquez ici et appuyer sur <kbd>Tab</kbd>.</p>
+		<SkipLink
+			label="Aller au contenu principal"
+			target="#main"
+		/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<div id="main">
+			<p>Le contenu principal est ici.</p>
+		</div>
+	</div>
+</template>
