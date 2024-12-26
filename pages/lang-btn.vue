6c6d62b643ad477c4cea88fb8aac0dcb22268abf
David FYON <davidfyon@gmail.com>
2024-12-26 14:17:47 +0100

    langBtn

--- pages/lang-btn.vue
+++ pages/lang-btn.vue
@@ -5,6 +5,14 @@ import { ref } from 'vue'
 const value = ref('fr')
 const items = ['fr', 'en', 'co']
 const ariaLabel = 'Choix de la langue'
+
+const vuetifyOptions = {
+	btn: {
+		color: 'primary',
+		variant: 'flat',
+		ripple: true,
+	},
+}
 </script>
 
 <template>
@@ -14,5 +22,31 @@ const ariaLabel = 'Choix de la langue'
 		:available-languages="items"
 		:aria-label="ariaLabel"
 		hide-down-arrow
+	/>
+
+	<br/>
+
+	<LangBtn
+		v-model="value"
+		:aria-label="ariaLabel"
+		available-languages="*"
+	/>
+
+	<br/>
+
+	<LangBtn
+		v-model="value"
+		:items="items"
+		:aria-label="ariaLabel"
+		hide-down-arrow
+	/>
+
+	<br/>
+
+	<LangBtn
+		v-model="value"
+		:items="items"
+		:aria-label="ariaLabel"
+		:vuetify-options="vuetifyOptions"
 	/>
 </template>
