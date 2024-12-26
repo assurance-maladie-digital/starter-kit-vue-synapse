4736b96d5e427616d60d0c3c5cbee8bc964e4e5b
David FYON <davidfyon@gmail.com>
2024-12-26 12:01:57 +0100

    BackBtn

---
+++ pages/back-btn.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { BackBtn } from '@cnamts/synapse'
+</script>
+
+<template>
+	<BackBtn/>
+
+	<br/>
+
+	<VSheet color="primary" class="pa-4">
+		<BackBtn dark />
+	</VSheet>
+
+	<br/>
+
+	<BackBtn hide-back-icon />
+
+	<br/>
+
+	<VSheet
+		color="primary"
+		class="pa-4"
+	>
+		<BackBtn
+			dark
+			hide-back-icon
+		/>
+	</VSheet>
+</template>
