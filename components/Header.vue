cc952eddc463c3b173cf53d8d8e0129d5bc1369a
adrien charrier <adrien.charrier@outlook.fr>
2025-01-02 17:20:37 +0100

    fix bugs + reformat menu

---
+++ components/Header.vue
@@ -0,0 +1,171 @@
+<script setup lang="ts">
+	import { ref } from 'vue'
+	import { HeaderBurgerMenu, HeaderBar, HeaderMenuSection, HeaderMenuItem, HeaderSubMenu } from '@cnamts/synapse'
+
+	const components = {
+		structure: [
+			{
+				label: 'HeaderBar',
+				to: '/header-bar',
+			},
+			{
+				label: 'FooterBar',
+				to: '/footer-bar',
+			},
+			{
+				label: 'SubHeader',
+				to: '/sub-header',
+			},
+			{
+				label: 'HeaderToolbar',
+				to: '/header-toolbar',
+			},
+			{
+				label: 'HeaderLoading',
+				to: '/header-loading',
+			},
+		],
+		layout: [
+			{
+				label: 'PageContainer',
+				to: '/page-container',
+			},
+		],
+		Navigation: [
+			{
+				label: 'SocialMediaLinks',
+				to: '/social-media-links',
+			},
+			{
+				label: 'SkipLink',
+				to: '/skip-link',
+			},
+		],
+		boutons: [
+			{
+				label: 'BackBtn',
+				to: '/back-btn',
+			},
+			{
+				label: 'BackToTopBtn',
+				to: '/back-to-top-btn',
+			},
+			{
+				label: 'CopyBtn',
+				to: '/copy-btn',
+			},
+			{
+				label: 'LangBtn',
+				to: '/lang-btn',
+			},
+			{
+				label: 'DownloadBtn',
+				to: '/download-btn',
+			},
+			{
+				label: 'FranceConnectBtn',
+				to: '/france-connect-btn',
+			},
+			{
+				label: 'UserMenuBtn',
+				to: '/user-menu-btn',
+			},
+		],
+		formulaires: [
+			{
+				label: 'SyTextField',
+				to: '/sy-text-field',
+			},
+			{
+				label: 'SySelect',
+				to: '/sy-select',
+			},
+			{
+				label: 'SyInputSelect',
+				to: '/sy-input-select',
+			},
+			{
+				label: 'SyBtnSelect',
+				to: '/sy-btn-select',
+			},
+			{
+				label: 'NirField',
+				to: '/nir-field',
+			},
+		],
+		données: [
+
+		],
+		feedback: [
+			{
+				label: 'SyAlert',
+				to: '/sy-alert',
+			},
+		]
+	}
+
+	const menuOpen = ref(false)
+</script>
+
+<template>
+	<HeaderBar
+		service-title="Titre du service"
+		service-sub-title="Description du service"
+	>
+		<template #menu>
+			<HeaderBurgerMenu v-model="menuOpen">
+				<HeaderMenuSection>
+					<HeaderMenuItem
+						@click="menuOpen = false"
+					>
+						<NuxtLink
+							to="/home"
+						>
+							Accueil
+						</NuxtLink>
+					</HeaderMenuItem>
+					<HeaderMenuItem
+						@click="menuOpen = false"
+					>
+						<NuxtLink
+							to="/test-inva"
+						>
+							Inva
+						</NuxtLink>
+					</HeaderMenuItem>
+					<HeaderMenuItem
+						v-for="(items, category) in components"
+						:key="category"
+					>
+						<HeaderSubMenu>
+							<template #title>
+								<span class="menu-category">
+									{{ category }}
+								</span>
+							</template>
+							<HeaderMenuSection>
+								<HeaderMenuItem
+									v-for="item in items"
+									:key="item.label"
+									@click="menuOpen = false"
+								>
+									<NuxtLink
+										:to="item.to"
+									>
+										{{ item.label }}
+									</NuxtLink>
+								</HeaderMenuItem>
+							</HeaderMenuSection>
+						</HeaderSubMenu>
+					</HeaderMenuItem>
+				</HeaderMenuSection>
+			</HeaderBurgerMenu>
+		</template>
+	</HeaderBar>
+</template>
+
+<style lang="scss" scoped>
+.menu-category {
+	text-transform: capitalize;
+}
+</style>
--- pages/test-inva.vue
+++ pages/test-inva.vue
@@ -1,90 +1,82 @@
 <template>
-	<V-Alert
-		class="mx-6 my-4"
-		border="start"
-		color="primary"
-		title="Changement de theme"
-		variant="tonal"
-	>
-		<p>Pour tester le changement de theme, il faut modifier la valeur theme dans le fichier : public/json/config.env.json (vuetify ou bootstrap)</p>
-	</V-Alert>
-	<PageContainer>
-		<BackBtn to="/"/>
-		<VBtn color="primary">
-			Primary
-		</VBtn>
-		<VBtn color="secondary">
-			secondary
-		</VBtn>
-		<VBtn color="info">
-			Info
-		</VBtn>
-		<VBtn color="success">
-			Success
-		</VBtn>
-		<VBtn color="warning">
-			Warning
-		</VBtn>
-		<VBtn color="error">
-			Error
-		</VBtn>
-		<VBtn color="accent">
-			Accent
-		</VBtn>
-		<VBtn color="light">
-			Light
-		</VBtn>
-		<VBtn color="dark">
-			Dark
-		</VBtn>
-		<VBtn color="risquePro">
-			Risque Pro
-		</VBtn>
-		<VBtn class="inva">
-			Violet inva
-		</VBtn>
+	<div>
+		<VAlert
+			class="mx-6 my-4"
+			border="start"
+			color="primary"
+			title="Changement de theme"
+			variant="tonal"
+		>
+			<p>Pour tester le changement de theme, il faut modifier la valeur theme dans le fichier : public/json/config.env.json (vuetify ou bootstrap)</p>
+		</VAlert>
+		<PageContainer>
+			<BackBtn to="/"/>
+			<VBtn color="primary">
+				Primary
+			</VBtn>
+			<VBtn color="secondary">
+				secondary
+			</VBtn>
+			<VBtn color="info">
+				Info
+			</VBtn>
+			<VBtn color="success">
+				Success
+			</VBtn>
+			<VBtn color="warning">
+				Warning
+			</VBtn>
+			<VBtn color="error">
+				Error
+			</VBtn>
+			<VBtn color="accent">
+				Accent
+			</VBtn>
+			<VBtn color="light">
+				Light
+			</VBtn>
+			<VBtn color="dark">
+				Dark
+			</VBtn>
+			<VBtn color="risquePro">
+				Risque Pro
+			</VBtn>
+			<VBtn class="inva">
+				Violet inva
+			</VBtn>
 
-		<br /><br />
+			<br /><br />
 
-		<SubHeader
-			sub-title-text="1 69 08 75 125 456 75"
-			title-text="Paul Dupont"
-		/>
+			<SubHeader
+				sub-title-text="1 69 08 75 125 456 75"
+				title-text="Paul Dupont"
+			/>
 
-		<br /><br />
+			<br /><br />
 
-		<v-alert type="success">
-			Message de succès
-		</v-alert>
-	</PageContainer>
-	<FooterBar
-		hide-a11y-link
-		hide-cgu-link
-		hide-cookies-link
-		hide-legal-notice-link
-		hide-sitemap-link
-	>
-		<p class="text-secondary mb-0">Contenu supplémentaire.</p>
-	</FooterBar>
+			<v-alert type="success">
+				Message de succès
+			</v-alert>
+		</PageContainer>
+		<FooterBar
+			hide-a11y-link
+			hide-cgu-link
+			hide-cookies-link
+			hide-legal-notice-link
+			hide-sitemap-link
+		>
+			<p class="text-secondary mb-0">Contenu supplémentaire.</p>
+		</FooterBar>
+	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import {PageContainer, FooterBar, BackBtn, SubHeader, NotFoundPage} from '@cnamts/synapse'
+<script setup lang="ts">
+import { PageContainer, FooterBar, BackBtn, SubHeader } from '@cnamts/synapse'
 
-export default defineComponent({
-	components: {
-		PageContainer,
-		FooterBar,
-		BackBtn,
-		SubHeader,
-		NotFoundPage
-	},
-})
 </script>
 
 <style lang="scss" scoped>
-@use '@cnamts/synapse/src/assets/tokens.scss';
+@use '@cnamts/synapse/assets/tokens.scss';
 
 .warning {
 	color: black !important;
