3cf302ee241e825272987b5d063e5b37897b6085
David FYON <davidfyon@gmail.com>
2025-01-15 10:49:09 +0100

    add external links ex

--- pages/external-links.vue
+++ pages/external-links.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
 import { ExternalLinks } from '@cnamts/synapse'
+import { mdiArrowTopRight } from '@mdi/js'
+
+const arrowIcon = mdiArrowTopRight
 
 const items = [
   {
@@ -15,6 +18,15 @@ const items = [
     "href": "https://www.twitter.com"
   }
 ];
+
+const options = {
+	btn: {
+		color: 'purple',
+	},
+	list: {
+		density: 'compact',
+	},
+}
 </script>
 
 <template>
@@ -24,10 +36,33 @@ const items = [
 		  position="bottom left"
 		  nudge-bottom="30px"
 		  />
+		<ExternalLinks
+			position="top left"
+			:items="items"
+			nudgeTop="50px"
+		/>
 		  <ExternalLinks
 			  :items="items"
 			  position="top right"
 			  nudge-bottom="30px"
 		  />
+		<ExternalLinks
+			position="top left"
+			nudgeTop="100px"
+			btn-text="Afficher les liens externes"
+			:items="[]"
+			:vuetifyOptions="options"
+		/>
+		<ExternalLinks
+			position="top left"
+			:items
+			nudgeTop="200px"
+		>
+			<template #link-icon>
+				<VIcon color="rgba(0, 0, 0, 0.5)">
+					{{ arrowIcon }}
+				</VIcon>
+			</template>
+		</ExternalLinks>
 	</div>
 </template>
